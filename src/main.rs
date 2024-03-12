1e5dccff7f299c63ac4ffb3b8a88180647c1744c
James McDonagh <jwmcdona@lakeheadu.ca>
2024-03-12 18:29:22 -0400

    save

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+enum ERROR
+{
+    None,
+    Duplicate,
+    Input
+}
+
+#[derive(Clone, Copy, PartialEq)]
 enum Player
 {
     None = 0,
@@ -9,55 +16,107 @@ enum Player
     O = -1 // second
 }
 
-#[derive(Debug)]
+impl std::fmt::Display for Player
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    {
+        match self
+        {
+            Player::None => write!(f, " "),
+            Player::X => write!(f, "X"),
+            Player::O => write!(f, "O")
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq)]
 struct Board
 {
-    to_play: Player,
+    to_move:Player,
     grid:[[Player; 3]; 3],
-    scores: [i8; 7],
+    line_scores:[i8; 8],
 }
 
 impl Board
 {
-    pub fn update_scores(&mut self, player:Player, cell:i8)
+    pub fn evaluate_board(&self) -> Player
     {
-        let value = player as i8;
-        match cell
-        {
-            1 => { self.scores[0] += value; self.scores[3] += value; self.scores[7] += value }
-            2 => { self.scores[1] += value; self.scores[3] += value }
-            3 => { self.scores[2] += value; self.scores[3] += value; self.scores[6] += value }
-            4 => { self.scores[0] += value; self.scores[4] += value; }
-            5 => { self.scores[1] += value; self.scores[4] += value; self.scores[6] += value; self.scores[7] += value }
-            6 => { self.scores[2] += value; self.scores[4] += value; }
-            7 => { self.scores[0] += value; self.scores[5] += value; self.scores[6] += value }
-            8 => { self.scores[1] += value; self.scores[5] += value; }
-            9 => { self.scores[2] += value; self.scores[5] += value; self.scores[7] += value }
-            _ => {}
-        }
-    }
-
-    pub fn evaluate(&self) -> Player
-    {
-        for score in self.scores
+        for score in self.line_scores
         {
             if score ==  3 { return Player::X };
             if score == -3 { return Player::O };
         }
         Player::None
     }
-}
 
-// * Note: for traits
-impl ToString for Board
-{
-    fn to_string(&self) -> String
+    pub fn make_move(&mut self, cell:i8)
     {
-        todo!()
+        let cell_index = cell - 1;
+        let row_index = cell_index / 3;
+        let col_index = cell_index % 3;
+
+        // !
+        if self.grid[row_index as usize][col_index as usize] != Player::None { return };
+
+        self.grid[row_index as usize][col_index as usize] = self.to_move;
+
+        let value = self.to_move as i8;
+        match cell
+        {
+            1 => { self.line_scores[0] += value; self.line_scores[3] += value; self.line_scores[7] += value }
+            2 => { self.line_scores[1] += value; self.line_scores[3] += value }
+            3 => { self.line_scores[2] += value; self.line_scores[3] += value; self.line_scores[6] += value }
+            4 => { self.line_scores[0] += value; self.line_scores[4] += value; }
+            5 => { self.line_scores[1] += value; self.line_scores[4] += value; self.line_scores[6] += value; self.line_scores[7] += value }
+            6 => { self.line_scores[2] += value; self.line_scores[4] += value; }
+            7 => { self.line_scores[0] += value; self.line_scores[5] += value; self.line_scores[6] += value }
+            8 => { self.line_scores[1] += value; self.line_scores[5] += value; }
+            9 => { self.line_scores[2] += value; self.line_scores[5] += value; self.line_scores[7] += value }
+            _ => {}
+        }
+
+        self.to_move = if self.to_move == Player::X { Player::O } else { Player::X };
     }
 }
 
-/*  0   1   2   6
+impl std::fmt::Display for Board
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    {
+        // let temp = "()".to_owned() + self.line_scores[0].to_string().as_str() + self.grid[0][0].to_string().as_str();
+        let r1 = format!(" {} │ {} │ {}\n", self.grid[0][0], self.grid[0][1], self.grid[0][2]);
+        let r2 = "───┼───┼───\n";
+        let r3 = format!(" {} │ {} │ {}\n", self.grid[1][0], self.grid[1][1], self.grid[1][2]);
+        let r4 = "───┼───┼───\n";
+        let r5 = format!(" {} │ {} │ {}", self.grid[2][0], self.grid[2][1], self.grid[2][2]);
+        let temp = format!("{}{}{}{}{}", r1, r2, r3, r4, r5);
+
+        write!(f, "{}", temp)
+    }
+}
+
+fn test_input(id:String) -> ERROR
+{
+    let mut played = [false; 9];
+    for cell in id.chars()
+    {
+        if !cell.is_numeric() || cell == '0'
+        {
+            return ERROR::Input;
+        }
+
+        let cell_index = cell.to_digit(10).unwrap() - 1;
+        if played[cell_index as usize] == true
+        {
+            return ERROR::Duplicate;
+        };
+        played[cell_index as usize] = true;
+    }
+    ERROR::None
+}
+
+/*
+    0   1   2   6
 ---===========----
     1 | 2 | 3   3
 ---===========----
@@ -68,45 +127,35 @@ impl ToString for Board
                 7
 */
 
-/*  0   1   2   3
+/*
+    0   1   2   6
 ---===========----
-    0 | 1 | 2   4
+    0 | 1 | 2   3
 ---===========----
-    3 | 4 | 5   5
+    3 | 4 | 5   4
 ---===========----
-    6 | 7 | 8   6
+    6 | 7 | 8   5
 ---===========----
                 7
 */
 
 fn build_board(id: String) -> Board
 {
-    let to_play = if id.len() % 2 == 0 { Player::X } else { Player::O };
-
-    let mut grid = [[Player::None; 3]; 3];
-
-    for (play, cell) in id.chars().enumerate()
+    let mut temp_board = Board
     {
-        let turn = play % 2;
-        let cell_value = cell.to_digit(10).unwrap() - 1;
-        let row = cell_value / 3;
-        let col = cell_value % 3;
-        grid[row as usize][col as usize] = if turn == 0 { Player::X } else { Player::O };
+        to_move:Player::X,
+        grid:[[Player::None; 3]; 3],
+        line_scores:[0;8]
+    };
+
+    for cell in id.chars()
+    {
+        temp_board.make_move(cell.to_digit(10).unwrap() as i8);
+        if temp_board.evaluate_board() != Player::None { return temp_board; }
     }
 
-    let mut scores = [0; 7];
-
-
-
-    return Board
-    {
-        to_play: to_play,
-        grid: grid,
-        scores: scores
-    }
+    return temp_board;
 }
-
-
 
 fn main() -> std::io::Result<()>
 {
@@ -116,10 +165,25 @@ fn main() -> std::io::Result<()>
     for line in reader.lines()
     {
         let current_line = line.unwrap();
-
         println!("{}", current_line);
-        let tempoard = build_board(current_line);
-        println!("{:?}", tempoard.grid);
+
+        let error = test_input(current_line.clone());
+
+        match error
+        {
+            ERROR::None => {
+                let temp_board = build_board(current_line);
+                println!("{}", temp_board);
+            },
+            ERROR::Input => {
+                println!("ERROR: Invalid Input");
+            },
+            ERROR::Duplicate => {
+                println!("ERROR: Duplicate Input");
+            }
+        }
+
+        println!("");
     }
 
     Ok(())
